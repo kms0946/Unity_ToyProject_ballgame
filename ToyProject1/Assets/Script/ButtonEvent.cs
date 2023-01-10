d346b2a4c7a94f3c017b805f682d18d9ea69e983
kms0946 <gghuly67@gmail.com>
2023-01-10 15:29:09 +0900

    Stage Select

--- ToyProject1/Assets/Script/ButtonEvent.cs
+++ ToyProject1/Assets/Script/ButtonEvent.cs
@@ -2,15 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonEvent : MonoBehaviour
 {
+    public GameManagement manager;
+
     public void StartButton()
     {
-        SceneManager.LoadScene("Example1_1");
+        SceneManager.LoadScene("StageSelect");
     }
     public void EndButton()
     {
             Application.Quit();
+    }
+    public void SelectButton()
+    {
+        SceneManager.LoadScene("Example1_1");
+    }
+    public void SelectButton2()
+    {
+        SceneManager.LoadScene("Example1_2");
     }
 }
